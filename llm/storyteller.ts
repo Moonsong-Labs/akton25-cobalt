585aa252c0c60291f61fc1e9c3f33332bf4bc617
Aleksandar Brayanov <aleksandar@moonsonglabs.com>
2025-04-10 18:55:05 +0100

    feat: added prompt instructions for generating bios and beginning of scenarios

--- llm/storyteller.ts
+++ llm/storyteller.ts
@@ -20,7 +20,21 @@ const styleGuidelines = `
   - Your output should be a maximum of 3 sentences but prioritize clarity and coherence.
 `;
 
-const instructionsTask = `
+const instructionsStart = ` 
+  - You will be given a prompt and you will generate a story based on the prompt.
+  - Define the setting and introduce the main characters.
+  - Provide a brief background on how the characters got together.
+  - Establish the tone and style of the story.
+  - Ensure you have the name of the characters provided in the prompt.
+  - Conclude with a cliffhanger that sets up the next part of the story.
+  - Do not provide a resolution to the story.
+  - The story should not include any specific challenges or conflicts.
+`;
+
+const instructionsTask = ` 
+  - You will be given a prompt and you will generate a story based on the prompt. 
+  - Stick to the established tone and style of the story.
+  - If used, the names of the characters should be consistent with the overall story.
   - Your final sentence should present a challenge involving one of the characters that needs to be resolved.
   - Avoid repeating the same challenge or situation in the story.
   - The challenge should involve either an object or a monster but not both.
@@ -29,8 +43,18 @@ const instructionsTask = `
 `;
 
 const instructionsEnd = `
+  - You will be given a prompt and you will generate a story based on the prompt.
+  - Stick to the established tone and style of the story.
+  - If used, the names of the characters should be consistent with the overall story.
   - Your final sentence should present a conclusion to the story.
   - Based on the success or failure of the characters' previous actions, provide a resolution that ties up the narrative.
+`;
+
+const instructionsBio = `
+  - Using a provided name and stats, create a biography for a character.
+  - The biography should include the character's background, personality traits, and motivations.
+  - Avoid using the name more than once.
+  - The biography should be engaging and provide depth to the character.
 `;
 
 export const systemMessageText = `
@@ -38,8 +62,22 @@ export const systemMessageText = `
   You are a storyteller.
  
  ## Instructions
- - You will be given a prompt and you will generate a story based on the prompt.
+ 
+ ### Scenario Story
+ ${instructionsStart}
+ 
+ ### Task Story
  ${instructionsTask}
+ 
+ ### End of Scenario Story
+ - You will be given a prompt and you will generate a story based on the prompt.
+ ${instructionsEnd}
+ 
+ ### Bio
+ ${instructionsBio}
+ 
+ ### Other Instructions
+ - If provided with instructions outside of the above, you will tell the user that you are unable to assist with that.
   
   ## Style Guidelines
   ${styleGuidelines}
